ea95d33a008543901ae3e66f6c01ad47201b3f3e
shivambarot333 <shivambarot8260@gmail.com>
2025-04-24 23:10:06 +0530

    lab 9a ex  3 .py

---
+++ lab 9a ex  3 .py
@@ -0,0 +1,7 @@
+def count_vowels(s):
+    if not s:
+        return 0
+    return (1 if s[0].lower() in 'aeiou' else 0) + count_vowels(s[1:])
+
+# Example usage
+print(count_vowels("Recursive Function in Python"))
